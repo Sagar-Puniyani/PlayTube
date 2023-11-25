6adb57375ebf212e121c32dca393ac6dfb5568e7
Sagar <sagarpuniyani@gmail.com>
2023-11-25 21:22:28 +0530

    First middleware

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,26 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if (!localFilePath) return null;
+
+        // upload the file 
+        const response = await cloudinary.uploader.upload(localFilePath , {
+            resource_type : "auto"
+        })
+        // file has been uploaded 
+        console.log(" CLOUDINARY : file has been uploaded");
+        console.log("CLOUDINARY : public url " , response.url);
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the local save temp file
+        console.error("CLOUDINARY : error " , error )
+        throw error;
+    }
+}
+
+cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+    { public_id: "olympic_flag" }, 
+    function(error, result) {console.log(result); });
