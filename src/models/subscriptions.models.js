5de83a9d03fe82497a891a032ae578bcee0b4c86
Sagar <sagarpuniyani@gmail.com>
2023-12-19 20:35:31 +0530

    Add subscription model

---
+++ src/models/subscriptions.models.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const subcriptionSchema = new mongoose.Schema(
+    {
+        subscriber : 
+        [
+            {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+            }
+        ],
+        channel : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    },
+    {
+        timestamps:  true 
+    }
+)
+
+export const Subscription = mongoose.model("Subscription" , subcriptionSchema );
