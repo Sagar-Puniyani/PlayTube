2fa32cd18136bb8005d781849f99f7e1ca305ab8
Sagar <sagarpuniyani@gmail.com>
2024-02-18 20:19:26 +0530

    Add imports and update getUserTweets function

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -1,4 +1,6 @@
+import mongoose,{ isValidObjectId } from "mongoose";
 import { Tweet } from "../models/tweet.models.js";
+import { User } from "../models/user.models.js"
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiRespone.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -7,7 +9,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
     const  owner  = req.user?._id;
-    console.log( "Owner : ", owner);
     if (! owner ){
         res.json(
             new ApiError(407 ,
@@ -51,6 +52,86 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const userId = req.params?.userId;
+
+    if ( !isValidObjectId(userId) ){
+        res.json(
+            new ApiError( 408 , "Object Id is Invalid " )
+        )
+    }
+
+    const tweets = await User.aggregate([
+        {
+            $match: {
+                _id : new mongoose.Types.ObjectId(userId),
+            },
+        },
+        {
+            $lookup: {
+                from: "tweets",
+                localField: "_id",
+                foreignField: "owner",
+                as: "tweets",
+                pipeline : [
+                    {
+                        $lookup: {
+                            from: "likes",
+                            localField: "_id",
+                            foreignField: "tweet",
+                            as: "likes"
+                        }
+                    },
+                    {
+                        $project: {
+                            content : 1 
+                        }
+                    }
+                ]
+            }
+            
+        },
+        {
+            $project: {
+                username : 1,
+                fullname : 1 ,
+                email : 1 ,
+                tweets : 1,
+                avatar : 1
+            }
+        },
+        {
+            $unwind: {
+                path: '$tweets',
+            }
+        },
+        {
+            $lookup: {
+                from:  "likes",
+                localField: "tweets._id",
+                foreignField: "tweet",
+                as: "likes"
+            }
+        },
+        {
+            $addFields: {
+                likes : {
+                    $size : { $ifNull : ['$likes' , []]}
+                }
+            }
+        }
+    ])
+
+    if (!tweets) {
+        res.json(
+            new ApiError(410 , "Error in Populating Database Call")
+        )
+    }
+
+    return res.json(
+        new ApiResponse(
+            200 , tweets , "Tweets and Their Likes"
+        )
+    )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
